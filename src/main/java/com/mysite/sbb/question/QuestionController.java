26b1ab0c132ae585097673c886ebdfe527d8f6a2
hyo-jung_SBS <rlagywnd05@gmail.com>
2025-08-06 15:50:11 +0900

    상세페이지 모델바인딩

--- src/main/java/com/mysite/sbb/question/QuestionController.java
+++ src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -28,6 +28,7 @@ public class QuestionController {
     @GetMapping(value = "/question/detail/{id}")
     public String detail(Model model, @PathVariable("id") Integer id) {
         Question question = questionService.getQuestion(id);
+        model.addAttribute("question", question);
         return "question_detail";
     }
 }
