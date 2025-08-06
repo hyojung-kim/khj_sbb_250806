2a99ab4dc2873933c89022a1a56d26edb7cb9db3
hyo-jung_SBS <rlagywnd05@gmail.com>
2025-08-06 15:47:26 +0900

    상세페이지 오류처리작성

--- src/main/java/com/mysite/sbb/question/QuestionController.java
+++ src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
@@ -22,5 +23,11 @@ public class QuestionController {
     @GetMapping("/")
     public String root() {
         return "redirect:/question/list";
+    }
+
+    @GetMapping(value = "/question/detail/{id}")
+    public String detail(Model model, @PathVariable("id") Integer id) {
+        Question question = questionService.getQuestion(id);
+        return "question_detail";
     }
 }
--- src/main/java/com/mysite/sbb/question/QuestionService.java
+++ src/main/java/com/mysite/sbb/question/QuestionService.java
@@ -1,9 +1,12 @@
 package com.mysite.sbb.question;
 
+import com.mysite.sbb.DataNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.zip.DataFormatException;
 
 @RequiredArgsConstructor
 @Service
@@ -13,5 +16,14 @@ public class QuestionService {
 
     public List<Question> getList() {
         return this.questionRepository.findAll();
+    }
+
+    public Question getQuestion(Integer id) {
+        Optional<Question> opQuestion = questionRepository.findById(id);
+        if(opQuestion.isPresent()){
+            return opQuestion.get();
+        }else {
+            throw new DataNotFoundException("question not found");
+        }
     }
 }
